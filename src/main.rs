1dc8fef23705aa2fd16ff0a9b79e25a3abfeb6ee
Jann Erhardt <CraftZockerLP@gmail.com>
2024-11-19 08:36:12 +0100

    Prepared router for nesting

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
 use tokio::net::TcpListener;
-
-use std::net::SocketAddr;
-
 use tower_http::trace::{self, TraceLayer};
 
 use tracing::Level;
 //use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Serialize)]
+struct Version {
+    version: String,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
         .with_target(false)
         .with_file(false)
-        .pretty()
+        .compact()
         .init();
 
-    let app = Router::new().route("/", get(hello_world)).layer(
+    let api_router = Router::new().route("/api/v1/", get(version)).layer(
         TraceLayer::new_for_http()
             .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
             .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
     );
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    tracing::info!("listening on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+    tracing::info!("listening on {:?}", listener.local_addr().unwrap());
+    axum::serve(listener, api_router).await.unwrap();
 }
 
-async fn hello_world() -> &'static str {
-    "Hello World!"
+async fn version() -> Json<Version> {
+    Json(Version {
+        version: "0.1.0".to_owned(),
+    })
 }
